295168915e364c52b512915855ce4fe4e7d769f7
Peter Kuhmann <info@peter-kuhmann.de>
2023-11-16 21:04:47 +0100

    Changed wording, drag and drop state

--- src/components/FcpXmlConverter.tsx
+++ src/components/FcpXmlConverter.tsx
@@ -1,53 +1,66 @@
 "use client";
 
 import clsx from "clsx";
-import { ChangeEventHandler, useCallback, useState } from "react";
+import { ChangeEventHandler, useCallback, useRef, useState } from "react";
 import { TextWriter, ZipReader } from "@zip.js/zip.js";
 import MarkerDescription from "@/components/MarkerDescription";
 import { useFcpXmlMarkers } from "@/hooks/fcp";
 
 export default function FcpXmlConverter() {
   const [fcpXmlContent, setFcpXmlContent] = useState<string | null>(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const fileSelected = useCallback((file: File | null) => {
+    if (file === null) {
+      setFcpXmlContent(null);
+      if (inputRef.current) {
+        inputRef.current.value = "";
+      }
+      return;
+    }
+
+    const zipReader = new ZipReader(file.stream());
+
+    zipReader
+      .getEntries()
+      .then((entries) => {
+        const fcpXmlFileEntry =
+          entries.find((entry) => entry.filename.endsWith("/Info.fcpxml")) ??
+          entries.find((entry) => entry.filename.endsWith(".fcpxml"));
+
+        if (!!fcpXmlFileEntry) {
+          // @ts-ignore
+          return fcpXmlFileEntry.getData(new TextWriter()).then((data) => {
+            setFcpXmlContent(data);
+          });
+        }
+      })
+      .catch((e) => {
+        setFcpXmlContent(null);
+        console.log({ e: e });
+      });
+  }, []);
 
   const onInputChange: ChangeEventHandler<HTMLInputElement> = useCallback(
     (event) => {
-      if (event.target.files && event.target.files.length > 0) {
-        const file = event.target.files[0];
-
-        const zipReader = new ZipReader(file.stream());
-
-        zipReader
-          .getEntries()
-          .then((entries) => {
-            const fcpXmlFileEntry =
-              entries.find((entry) =>
-                entry.filename.endsWith("/Info.fcpxml"),
-              ) ?? entries.find((entry) => entry.filename.endsWith(".fcpxml"));
-
-            if (!!fcpXmlFileEntry) {
-              // @ts-ignore
-              return fcpXmlFileEntry.getData(new TextWriter()).then((data) => {
-                setFcpXmlContent(data);
-              });
-            }
-          })
-          .catch(() => {
-            setFcpXmlContent(null);
-          });
-      } else {
-        setFcpXmlContent(null);
-      }
+      fileSelected(
+        event.target.files && event.target.files.length > 0
+          ? event.target.files[0]
+          : null,
+      );
     },
-    [],
+    [fileSelected],
   );
 
   const onResetHandler = useCallback(() => {
-    setFcpXmlContent(null);
-  }, []);
+    fileSelected(null);
+  }, [fileSelected]);
 
   return (
     <div>
       <input
+        ref={inputRef}
         id="fcpXmlInput"
         type={"file"}
         className={"hidden"}
@@ -55,19 +68,47 @@ export default function FcpXmlConverter() {
       />
 
       <label
+        onDragEnter={(e) => {
+          e.stopPropagation();
+          e.preventDefault();
+          setIsDragging(true);
+        }}
+        onDragLeave={(e) => {
+          e.stopPropagation();
+          e.preventDefault();
+          setIsDragging(false);
+        }}
+        onDragOver={(e) => {
+          e.stopPropagation();
+          e.preventDefault();
+        }}
+        onDrop={(e) => {
+          e.stopPropagation();
+          e.preventDefault();
+          setIsDragging(false);
+          if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
+            fileSelected(e.dataTransfer.files[0]);
+          }
+        }}
         htmlFor="fcpXmlInput"
         className={clsx(
           "block mb-8",
-          "bg-my-purple text-gray-50",
-          "dark:bg-gray-50 dark:text-gray-950",
           "px-24 py-12",
           "text-center cursor-pointer",
+          {
+            "bg-my-purple text-gray-50": !isDragging,
+            "bg-my-lemon text-gray-950": isDragging,
+            "dark:bg-gray-50 dark:text-gray-950": !isDragging,
+            "dark:bg-my-lemon dark:text-gray-950": isDragging,
+          },
           { hidden: !!fcpXmlContent },
         )}
       >
-        Select FCP XML file
-        <br />
-        or drop it here
+        {isDragging ? (
+          <>Drop it like it&apos;s hot 🍕</>
+        ) : (
+          <>Select FCP XML export</>
+        )}
       </label>
 
       {!!fcpXmlContent && (
