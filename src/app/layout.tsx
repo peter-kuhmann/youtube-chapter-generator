f2cd972819e146f98375e82d2ad3dabaef62a064
Peter Kuhmann <info@peter-kuhmann.de>
2024-09-14 20:11:52 +0200

    Added Umami tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,13 +12,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>
-        <Navigation />
+    <head>
+        <meta charSet="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <script
+            defer
+            src="https://umami.peter-kuhmann.de/script.js"
+            data-website-id="b1e81371-b680-422b-bfa7-3f1438b46d18"
+        />
+    </head>
+    <body className={font.className}>
+    <Navigation/>
 
-        <>{children}</>
+    <>{children}</>
 
-        <Footer />
-      </body>
+    <Footer/>
+    </body>
     </html>
   );
 }
